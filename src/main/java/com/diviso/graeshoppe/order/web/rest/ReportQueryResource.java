3232e74c3b5ff1103f44a86f5d7a2fd5f5d14d2e
mayabytatech <maya.k.k@lxisoft.com>
2019-08-22 15:51:47 +0530

    check

--- src/main/java/com/diviso/graeshoppe/order/web/rest/ReportQueryResource.java
+++ src/main/java/com/diviso/graeshoppe/order/web/rest/ReportQueryResource.java
@@ -264,9 +264,9 @@ public class ReportQueryResource {
 		List<Order> storeBased = new ArrayList<Order>();
 
 		Double total = 0.0;
-
+		log.info(".........................................................." + storeBased.size());
 		for (int i = 0; i < storeBased.size(); i++) {
-			log.info(".................." + Date.from(order.get(i).getDate()).toString());
+			log.info("..............................................." + Date.from(order.get(i).getDate()).toString());
 			if (Date.from(order.get(i).getDate()).toString().substring(4, 10).equals("Aug 20")) {
 				log.info(".................." + order.get(i));
 				order.add(order.get(i));
