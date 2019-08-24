b9cf8cebefba837970ade58a3f32161f74378189
abilash-diviso <abilash.s@lxisoft.com>
2019-08-24 10:14:32 +0530

    added new ethod to fetch order using statusname and oderid issue fixed compilation

--- src/main/java/com/diviso/graeshoppe/order/web/rest/ReportQueryResource.java
+++ src/main/java/com/diviso/graeshoppe/order/web/rest/ReportQueryResource.java
@@ -19,6 +19,7 @@ import java.sql.Date;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
